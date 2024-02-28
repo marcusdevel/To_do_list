db7c3280eee949c379d9e258d783b5b60d5160af
Marcus <marcusviniciusjp@hotmail.com>
2024-02-28 16:55:43 -0300

    criações das classes header(imagem como background), App-header(agrupamento do h1 com a image icon sun).

--- To_do_list/src/App.jsx
+++ To_do_list/src/App.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import bgDesktopDark from "./assets/img/bg-desktop-dark.jpg";
+import icon from "./assets/img/icon-sun.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello-World</h1>
+      <div className="App">
+        <div className="header">
+          <div className="App-header">
+            <h1 className="title">To do</h1>
+            <img src={icon} alt="Icon Sun" className="icon" />
+          </div>
+          <input type="select" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
